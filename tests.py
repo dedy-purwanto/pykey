1b668680f75621ccfea1af6cbacaec80c0e74972
Dedi Purwanto <kecebongsoft@gmail.com>
2014-10-12 23:26:19 +0800

    Trying to fix module path

--- tests.py
+++ tests.py
@@ -2,17 +2,17 @@ import os
 import unittest
 import ConfigParser
 
-from pykey import pykey
+from pykey.pykey import CONFIG_FILENAME, get_config, write_config, config_has_default
 
 class TestMixin(object):
 
     def setUp(self):
-        if os.path.isfile(pykey.CONFIG_FILENAME):
-            os.remove(pykey.CONFIG_FILENAME)
+        if os.path.isfile(CONFIG_FILENAME):
+            os.remove(CONFIG_FILENAME)
 
     def cleanUp(self):
-        if os.path.isfile(pykey.CONFIG_FILENAME):
-            os.remove(pykey.CONFIG_FILENAME)
+        if os.path.isfile(CONFIG_FILENAME):
+            os.remove(CONFIG_FILENAME)
 
 class TestVault(unittest.TestCase):
 
@@ -45,38 +45,38 @@ class TestPassword(unittest.TestCase):
 class TestConfig(TestMixin, unittest.TestCase):
 
     def test_write_config(self):
-        old_config = pykey.get_config()
-        new_config = pykey.write_config(old_config)
+        old_config = get_config()
+        new_config = write_config(old_config)
         self.assertTrue(isinstance(new_config, ConfigParser.RawConfigParser))
 
     def test_edit_config(self):
-        old_config = pykey.get_config()
+        old_config = get_config()
         old_config.add_section('main')
         old_config.set('main', 'vault', 'default.json')
         old_config.set('main', 'key', 'default.key')
-        pykey.write_config(old_config)
+        write_config(old_config)
 
-        new_config = pykey.get_config()
+        new_config = get_config()
         self.assertTrue(new_config.has_section('main'))
         self.assertEqual(new_config.get('main', 'vault'), 'default.json')
         self.assertEqual(new_config.get('main', 'key'), 'default.key')
 
     def test_get_config(self):
-        config = pykey.get_config()
+        config = get_config()
         self.assertTrue(isinstance(config, ConfigParser.RawConfigParser))
 
     def test_config_has_no_default(self):
-        config = pykey.get_config()
-        self.assertFalse(pykey.config_has_default(config))
+        config = get_config()
+        self.assertFalse(config_has_default(config))
 
     def test_config_has_default(self):
-        old_config = pykey.get_config()
+        old_config = get_config()
         old_config.add_section('main')
         old_config.set('main', 'vault', 'default.json')
         old_config.set('main', 'key', 'default.key')
-        pykey.write_config(old_config)
-        config = pykey.get_config()
-        self.assertTrue(pykey.config_has_default(config))
+        write_config(old_config)
+        config = get_config()
+        self.assertTrue(config_has_default(config))
 
 if __name__ == '__main__':
     unittest.main()
